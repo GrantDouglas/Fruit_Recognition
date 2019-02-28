b38bb9a7a5f3e43fe451e69e0a0e6ab766b7f4a2
Grant <grantdouglashand@outlook.com>
2019-02-28 13:45:37 -0500

    some groundwork to start off

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fruit_Ripenes_Recognition
 Determines if a fruit is ripe or not
+
+reference article(s):
+http://thesai.org/Downloads/Volume7No5/Paper_69-Detection_and_Counting_of_On_Tree_Citrus_Fruit.pdf
---
+++ a2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+
+import cv2
+import numpy as np
+import PIL
+
+
+def openImage(fname):
+	return np.asarray(PIL.Image.open(fname))
+
+
+
+def shadowReduce:
+	print("do this")
+
+
+def kMeansAlgo:
+	print("do this too")
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	
