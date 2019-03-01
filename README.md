c10a1114ac5cb6f5a004730c804834967ffd0bec
Grant <grantdouglashand@outlook.com>
2019-02-28 20:21:56 -0500

    intermediary for shadow removal, moving to desktop

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Determines if a fruit is ripe or not
 
 reference article(s):
 http://thesai.org/Downloads/Volume7No5/Paper_69-Detection_and_Counting_of_On_Tree_Citrus_Fruit.pdf
+
+
+Image references:
+https://wikifarmer.com/growing-orange-trees-for-profit/
+
+
+right number of fruit (how many did it find vs how many are there)
+location of the fruit (how close are they to image compared to how far they actually are)
+ripeness (if needed)
--- a2.py
+++ a2.py
@@ -3,26 +3,78 @@
 
 import cv2
 import numpy as np
-import PIL
+np.set_printoptions(threshold=np.nan)
+from PIL import Image, ImageCms, ImageEnhance
+from skimage import color
 
 
 def openImage(fname):
-	return np.asarray(PIL.Image.open(fname))
+	return Image.open(fname).convert('RGB')
 
 
 
-def shadowReduce:
+def shadowReduce(image):
 	print("do this")
 
+	# # create colour spaces
+	# srgb_p = ImageCms.createProfile("sRGB")
+	# lab_p  = ImageCms.createProfile("LAB")
 
-def kMeansAlgo:
+	# # build colour space conversions
+	# rgb2lab = ImageCms.buildTransformFromOpenProfiles(srgb_p, lab_p, "RGB", "LAB")
+	# Lab = ImageCms.applyTransform(image, rgb2lab)
+
+	# # cather the images in the LAB colour spaces
+	# L, a, b = Lab.split()
+	# L.save('L.png')
+	# a.save('a.png')
+	# b.save('b.png')
+
+
+	# print(Lab)
+	
+	enhancer = ImageEnhance.Brightness(image)
+	Lab = enhancer.enhance(1.3)
+
+
+	# # combined = ','.join(fin_L, a, b)
+
+	# lab2rgb = ImageCms.buildTransformFromOpenProfiles(lab_p, srgb_p, "LAB", "RGB")
+	# Lab2 = ImageCms.applyTransform(Lab, lab2rgb)
+
+
+	# initial = color.rgb2hsv(np.asarray(image))
+
+
+	# initial[:, :, 2] += 10
+
+
+
+	# final = color.hsv2rgb(initial)
+
+	# img = Image.fromarray(final, 'RGB')
+
+	# img.save('final.png')
+
+
+
+
+	Lab.save('L_Change.png')
+
+
+
+
+
+def kMeansAlgo():
 	print("do this too")
 
 
 
-
-
-
-
 if __name__ == '__main__':
-	
+	image = openImage("./images/orange1.jpg")
+
+	shadowReduce(image)
+
+
+
+
