011f875d93c354392bba4ca562f9e9af9ec8bb80
Grant <grantdouglashand@outlook.com>
2019-03-03 22:03:23 -0500

    fixed the algogit add .

--- a2.py
+++ a2.py
@@ -1,4 +1,4 @@
-#!/usr/local/bin/python
+#!/usr/bin/python3
 
 
 import cv2
@@ -44,6 +44,8 @@ def varianceFilter(img):
 
     ndi = NDI(img)
 
+    print(img)
+
 
 
     r_var = ndimage.generic_filter(r, np.var, size=3)
@@ -66,43 +68,43 @@ def varianceFilter(img):
     for k in range(0, img.shape[0]):
         for i in range(0, img.shape[1]):
             if mean[k, i] <= 90:
-                mean[k, i] = 255
-            else:
                 mean[k, i] = 0
+            else:
+                mean[k, i] = 255
 
     cv2.imwrite("mean.png", mean)
 
 
-    # convert image to YCrBr color space
-    ycrbr = cv2.cvtColor(img, 36)
-    y, cr, br = cv2.split(ycrbr)
+    # convert image to YCrcb color space
+    ycrcb = cv2.cvtColor(img, 36)
+    y, cr, cb = cv2.split(ycrcb)
 
     # cr threshold
     for k in range(0, img.shape[0]):
         for i in range(0, img.shape[1]):
-            if cr[k, i] >= 150:
+            if cr[k, i] >= 140:
                 cr[k, i] = 0
             else:
                 cr[k, i] = 255
 
     cv2.imwrite("cr.png", cr)
 
-    # br threshold
+    # cb threshold
     for k in range(0, img.shape[0]):
         for i in range(0, img.shape[1]):
-            if br[k, i] > 100:
-                br[k, i] = 0
+            if cb[k, i] > 45 and cb[k, i] < 100:
+                cb[k, i] = 255
             else:
-                br[k, i] = 255
+                cb[k, i] = 0
 
-    cv2.imwrite("br.png", br)
+    cv2.imwrite("cb.png", cb)
 
 
     # final = np.zeros(shape=(img.shape[0], img.shape[1]))
 
     # for k in range(0, img.shape[0]):
     #     for i in range(0, img.shape[1]):
-    #         val = [ndi[k, i], mean[k, i], cr[k, i], br[k, i]]
+    #         val = [ndi[k, i], mean[k, i], cr[k, i], cb[k, i]]
     #         if len(set(val)) == 1:
     #             pixel = 0
     #         else:
@@ -114,7 +116,7 @@ def varianceFilter(img):
     fin_ndi = cv2.imread("ndi.png", cv2.IMREAD_GRAYSCALE)
     fin_mean = cv2.imread("mean.png", cv2.IMREAD_GRAYSCALE)
     fin_cr = cv2.imread("cr.png", cv2.IMREAD_GRAYSCALE)
-    fin_br = cv2.imread("br.png", cv2.IMREAD_GRAYSCALE)
+    fin_cb = cv2.imread("cb.png", cv2.IMREAD_GRAYSCALE)
 
 
     fin_ndi[fin_ndi < 255] = 0
@@ -126,8 +128,8 @@ def varianceFilter(img):
     fin_cr[fin_cr < 255] = 0
     fin_cr[fin_cr == 255] = 1
 
-    fin_br[fin_br < 255] = 0
-    fin_br[fin_br == 255] = 1
+    fin_cb[fin_cb < 255] = 0
+    fin_cb[fin_cb == 255] = 1
 
     res = np.zeros(shape=(img.shape[0], img.shape[1]))
 
@@ -137,36 +139,49 @@ def varianceFilter(img):
         for i in range(0, img.shape[1]):
 
 
-            if fin_ndi[k,i] == 1 and fin_mean[k,i] == 1 and fin_cr[k,i] == 1 and fin_br[k,i] == 1:
-               res[k, i] = 255
-            else:
+            if fin_ndi[k, i] == 0 and fin_mean[k, i] == 0 and fin_cr[k, i] == 0 and fin_cb[k, i] == 1:
                res[k, i] = 0
+            else:
+               res[k, i] = 255
 
 
-   #res = np.logical_and.reduce([fin_ndi, fin_mean, fin_cr, fin_br])
+   #res = np.logical_and.reduce([fin_ndi, fin_mean, fin_cr, fin_cb])
    #res.dtype = 'uint8'
 
    #print(res)
 
     cv2.imwrite("final.png", res)
 
+    for k in range(0, img.shape[0]):
+        for i in range(0, img.shape[1]):
+            if res[k, i] == 0:
+                b[k, i] = 0
+                g[k, i] = 0
+                r[k, i] = 0
+
+
+    final = cv2.merge((b, g, r))
+
+    cv2.imwrite("yay.png", final)
+
+
 
 
     # bin_ndi = cv2.threshold(fin_ndi, 255, 1, cv2.THRESH_BINARY)
     # bin_mean = cv2.threshold(fin_mean, 255, 1, cv2.THRESH_BINARY)
     # bin_cr = cv2.threshold(fin_cr, 255, 1, cv2.THRESH_BINARY)
-    # bin_br = cv2.threshold(fin_br, 255, 1, cv2.THRESH_BINARY)
+    # bin_cb = cv2.threshold(fin_cb, 255, 1, cv2.THRESH_BINARY)
 
 
     # print(bin_ndi)
 
     # res1 = cv2.bitwise_and(bin_ndi, bin_mean)
     # res2 = cv2.bitwise_and(res1, bin_cr)
-    # res3 = cv2.bitwise_and(res2, bin_br)
+    # res3 = cv2.bitwise_and(res2, bin_cb)
 
 
 
-    # final = cr + br + mean + ndi
+    # final = cr + cb + mean + ndi
 
     # cv2.imwrite("final.png", res3)
 
