a84ee8d308dcd6980b4401c67ea556b89e01ad35
Marshall Asch <maasch@rogers.com>
2019-03-05 17:19:11 -0500

    some more changes

--- circles.py
+++ circles.py
@@ -17,10 +17,10 @@ import numpy as np
 def circleCount(fname):
 
 	img = cv2.imread("masks/" + fname,0)
-	kernel = np.ones((10,10),np.uint8)
+	#kernel = np.ones((10,10),np.uint8)
 
-	img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-	cv2.imwrite("open.png", img)
+	#img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+	#cv2.imwrite("open.png", img)
 
 
 	img = cv2.medianBlur(img,5)
@@ -35,19 +35,20 @@ def circleCount(fname):
 	count = 0
 	for i in circles[0,:]:
 
- 		if i[2] < 500:
+ 		if  i[2] < 500:
 
 			mask = np.zeros(shape=(img.shape[0], img.shape[1]))
 			cv2.circle(mask,(i[0],i[1]),i[2],255, -1)
+			cv2.imwrite("mask.png", mask)
 			vals = countWhite(mask, img, (i[0],i[1]),i[2])
 
 			if vals[1] >  vals[0] / 2:
 				count += 1
 				# draw the outer circle
-				cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),2)
+				cv2.circle(cimg,(i[0],i[1]),i[2],(0,255,0),10)
 			    # draw the center of the circle
 
-				cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),3)
+				cv2.circle(cimg,(i[0],i[1]),2,(0,0,255),5)
 
 
 
@@ -57,21 +58,20 @@ def circleCount(fname):
 
 def countWhite(mask, img, center, radius):
 
-	numTotal = 3.14 * radius**2
+	numTotal = int(3.14 * radius**2)
 	numWhite = 0
 
 
-	lowerbound_out = max(center[1] - radius, 0)
+	lowerbound_out = max(int(center[1]) - radius, 0)
 	upperbound_out = min(center[1] + radius, img.shape[0])
 
-	lowerbound_in = max(center[0] - radius, 0)
+	lowerbound_in = max(int(center[0]) - radius, 0)
 	upperbound_in = min(center[0] + radius, img.shape[1])
-
 
 	for k in range(lowerbound_out, upperbound_out):
 		for i in range(lowerbound_in, upperbound_in):
 
-			if (mask[k,i] == 255 and img[k,i] == 255):
+			if (mask[k,i] == 255 and img[k,i] == 0):
 				numWhite += 1
 
 
@@ -84,6 +84,6 @@ if __name__ == '__main__':
 
     # imageList = os.listdir("./images/")
 
-    imageList = "citrus4_mask_invert.jpg"
+    imageList = "citrus1_mask.jpg"
 
     circleCount(imageList)
